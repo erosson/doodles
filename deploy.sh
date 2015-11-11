a40a72d456c066024dd9525fe7a39ed7da289126
Evan Rosson <github@erosson.org>
2015-11-10 20:02:35 -0800

    deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,16 @@
 rm -rf dist/
 mkdir dist
 
+echo '# doodles' > README.md
+echo "quick  little js experiments that don't deserve their own repo or build scripts" >> README.md
+echo "" >> README.md
+for f in `ls -t *.html`; do
+  bf=`basename $f`
+  if [ "$bf" != "index.html" ]; then
+    echo "* [$bf](https://erosson.github.io/doodles/$bf)" >> README.md
+  fi
+done
+
 set +e
 cp -rp * dist
 set -e
